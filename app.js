4b495f683697593d15b3d2528ef22f43007b30a5
Edelyn <edelyn@Edelyns-MacBook-Pro.local>
2022-03-25 00:37:13 +0800

    wordle

--- app.js
+++ app.js
@@ -27,3 +27,67 @@ $(window).on('load', () => {
         else {switchButton.innerText='Let there be dark'} //inner text or HTML both work, HTML works because there is no HTML other than the text
     });
 });
+
+function lowestCommonMultiple() {
+    let num1 = document.forms["lcmform"]["num1"].value;
+    let num2 = document.forms["lcmform"]["num2"].value;
+    let min = (num1 > num2) ? num1 : num2;
+
+    while (true) {
+    if (min % num1 == 0 && min % num2 == 0) {
+        console.log(`The LCM of ${num1} and ${num2} is ${min}`);
+        break;}
+        min++;
+    }
+    //changing the tag with id lcmAnwer to the calculation answer
+    let answer = document.getElementById("lcmAnswer");
+    answer.innerHTML = min ;
+};
+
+function wordle() {
+    let target = document.forms["wordleClone"]["targetWord"].value;
+    let guess = document.forms["wordleClone"]["guessWord"].value;
+
+    //convert text to string 
+    const targetArray = target.split("");
+    const guessArray = guess.split("");
+    let wordleAnswer = "";
+    const numText = ["first","second","third","fourth","fifth"];
+
+
+    //check if letter matches in the correct position
+        // if yes then say that letter is in correct position
+        // else check if letter is in word
+                // if yes say appear in word but different position
+                // else say it doesnt appear in word
+
+    for (let i = 0; i < 5; i++)   {
+        if (targetArray[i] == guessArray[i]) {
+            wordleAnswer += "The " + numText[i] + " letter is in the correct position."; 
+        } else if (targetArray.includes(guessArray[i]) && calcOccurances(targetArray,  guessArray[i]) == 1)  {
+            wordleAnswer += "The " + numText[i] + " letter appears in the word, but in a different position.";
+        } else {
+            wordleAnswer += "The " + numText[i] + " letter doesn't appear in the word.";
+        }
+    };
+
+    //console.log(wordleAnswer);
+
+    //changing the tag with id lcmAnwer to the calculation answer
+    let finalWordleAnswer = document.getElementById("wordleAnswer");
+    finalWordleAnswer.innerHTML = wordleAnswer;
+
+}
+
+function calcOccurances(targetArray, letter) {
+    let counting = 0;
+
+    for ( let i=0; i<5; i++) {
+        if (letter == targetArray[i]) {
+            counting += 1;
+        } else { counting += 0;}
+    }
+    //console.log (counting);
+
+    return counting;
+}
